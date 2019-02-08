5f4595918906211cfbbdfab92c94fc9d582b5d5b
Caleb Kahan <ckahan00@stuy.edu>
2019-02-08 14:37:48 -0500

    list twice as short

--- QueenBoard.java
+++ QueenBoard.java
@@ -5,7 +5,7 @@ public class QueenBoard{
   private int [][] board;
   private List<Integer> illegalRows;
   private List<Integer> illegalColumns;
-  private List<String> illegalLines;
+  private List<String> illegalPoints;
 
 	public QueenBoard(int size){
 		board = new int[size][size];
@@ -13,6 +13,8 @@ public class QueenBoard{
 
   private boolean addQueen(int r, int c){
 		board[r][c]=1;
-
+    illegalRows.add(r);
+    illegalColumns.add(c);
 	}
+
 }
