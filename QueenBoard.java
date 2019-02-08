7e00046e4c2ca2129890585ce5d0779e06e331ad
Caleb Kahan <ckahan00@stuy.edu>
2019-02-08 14:20:15 -0500

    variables + constructor

--- QueenBoard.java
+++ QueenBoard.java
@@ -2,6 +2,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class QueenBoard{
+  private int [][] board;
+	private List<String> queenBoard;
 
+	public QueenBoard(int size){
+		board = new int[size][size]
+		queenBoard = new ArrayList<String>();
+	}
 
 }
