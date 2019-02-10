25882834550802595f098ca0a00d6995464f43e2
Caleb Kahan <ckahan00@stuy.edu>
2019-02-10 16:54:57 -0500

    solve works for queenboardalternate. 3 more to go

--- QueenBoardAlternateDriver.java
+++ QueenBoardAlternateDriver.java
@@ -1,6 +1,6 @@
 public class QueenBoardAlternateDriver{
 	public static void main(String [] args){
 		QueenBoardAlternate chess = new QueenBoardAlternate(8);
-		System.out.println(chess.solve());
+		System.out.println(chess.countSolutions());
 	}
 }
