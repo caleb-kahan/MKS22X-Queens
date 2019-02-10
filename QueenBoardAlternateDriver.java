d19539136711f623ef1d7580f1b74929b4f8360b
Caleb Kahan <ckahan00@stuy.edu>
2019-02-10 16:10:02 -0500

    switch

--- QueenBoardAlternateDriver.java
+++ QueenBoardAlternateDriver.java
@@ -1,6 +1,6 @@
 public class QueenBoardAlternateDriver{
 	public static void main(String [] args){
-		QueenBoard chess = new QueenBoard(8);
+		QueenBoard chess = new QueenBoardAlternate(8);
 		System.out.println(chess.countSolutions());
 	}
 }
