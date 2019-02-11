6df9b5d11fbb546a8d61dbfa896cb5c9069adff3
Caleb Kahan <ckahan00@stuy.edu>
2019-02-10 21:11:56 -0500

    simplified remove queen param

--- QueenBoardAlternate.java
+++ QueenBoardAlternate.java
@@ -19,16 +19,17 @@ public class QueenBoardAlternate{
 		illegalPoints.add(r + "" +c);
 		return true;
 	}
+
 	private boolean findSlope(int x, int y, String xy){
-		double a = xy.charAt(0);
-		double b = xy.charAt(1);
-		double slope = (y-b)/(x-a);
-		if(Math.abs(slope)==1 || slope ==0) return false;
+		int a = xy.charAt(0);
+		int b = xy.charAt(1);
+		if( x == a || y == b || Math.abs(y-b) == Math.abs(x-a)) return false;
 		return true;
 	}
-	private boolean removeQueen(int r , int c){
-		if(illegalPoints.contains(r + "" + c)){
-			illegalPoints.remove(r + "" + c);
+
+	private boolean removeQueen(String xy){
+		if(illegalPoints.contains(xy)){
+			illegalPoints.remove(xy);
 			return true;
 		}
 		return false;
@@ -36,55 +37,55 @@ public class QueenBoardAlternate{
 
 	public String toString(){
 		String str = "";
-		for(int i=0;i<size;i++){
-			for(int j=0;j<size;j++){
-				if(illegalPoints.contains(i+""+ j)){
-					str+="Q ";
+		for(int i=0; i<size; i++){
+			for(int j=0; j<size; j++){
+				if(illegalPoints.contains(i + "" + j)){
+					str += "Q ";
 				}
 				else {
-					str+="_ ";
+					str += "_ ";
 				}
 			}
-			str+="\n";
+			str += "\n";
 		}
 		return str;
 	}
 
 	public boolean solve(){
-		if(illegalPoints.size()>0)throw new IllegalStateException("There is already a Queen");
-		return 1==solveH(0,false,0);
+		if(illegalPoints.size() > 0) throw new IllegalStateException("There is already a Queen");
+		return 1 == solveH(0, false, 0);
 
 	}
-	private int solveH(int row,boolean integer, int sum){
-		if(illegalPoints.size()==size){
+	private int solveH(int row, boolean retNumSolutions, int sum){
+		if(illegalPoints.size() == size){
 			sum++;
-			if(integer==false)
-			return sum;
+			if(retNumSolutions==false)
+				return sum;
 			else{
-				String str = illegalPoints.get(illegalPoints.size()-1);
-				int nextRow = str.charAt(0)+1;
-				if(! removeQueen(str.charAt(0),str.charAt(1)))return -1;
-				return solveH(nextRow,integer,sum);
+				String str = illegalPoints.get(illegalPoints.size() - 1);
+				int nextRow = str.charAt(0) + 1;
+				if(! removeQueen(str)) return -1;
+				return solveH(nextRow, retNumSolutions, sum);
 			}
 		}
 		int column = illegalPoints.size();
-		int i=row;
-		while(i<size){
-			if(addQueen(i,column))break;
+		int i = row;
+		while(i < size){
+			if(addQueen(i, column)) break;
 			i++;
 		}
-		if(i==size){
-			if(column==0)return sum;
+		if(i == size){
+			if(column == 0) return sum;
 			String str = illegalPoints.get(illegalPoints.size()-1);
-			int newStart = str.charAt(0)+1;
-			removeQueen(str.charAt(0),str.charAt(1));
-			return solveH(newStart,integer,sum);
+			int newStart = str.charAt(0) + 1;
+			removeQueen(str);
+			return solveH(newStart, retNumSolutions, sum);
 		}
 		else
-		return solveH(0,integer,sum);
+			return solveH(0, retNumSolutions, sum);
 	}
 	public int countSolutions(){
-		if(illegalPoints.size()>0)throw new IllegalStateException("There is already a Queen");
-		return solveH(0,true, 0);
+		if(illegalPoints.size() > 0) throw new IllegalStateException("There is already a Queen");
+		return solveH(0, true, 0);
 	}
 }
