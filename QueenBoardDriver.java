0bc509c49c2ef2b9d17dfdc04be5630d3578a359
Caleb Kahan <ckahan00@stuy.edu>
2019-02-11 19:45:55 -0500

    EVERYTHING WORKSKADSKAKD

--- QueenBoardDriver.java
+++ QueenBoardDriver.java
@@ -2,6 +2,7 @@ public class QueenBoardDriver{
 	public static void main(String [] args){
 		QueenBoard chess = new QueenBoard(8);
 		System.out.println(chess.solve());
-		System.out.println(chess.countSolutions());
+		System.out.println(chess);
+		//System.out.println(chess.countSolutions());
 	}
 }
