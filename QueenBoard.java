e70376c13f8e667b43a58cfbd493913b7608dd99
Caleb Kahan <ckahan00@stuy.edu>
2019-02-11 19:53:24 -0500

    combined files

--- QueenBoard.java
+++ QueenBoard.java
@@ -1,11 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/*Commented Out stuff is your marking stuff.*/
+
 public class QueenBoard{
-  private int [][] board;
+  //private int [][] board;
+  private List<String> illegalPoints;
+	private int size;
 
   public QueenBoard(int size){
-    board = new int[size][size];
+    //board = new int[size][size];
+    illegalPoints = new ArrayList<String>();
+		this.size=size;
   }
 
-  private boolean addQueen(int r, int c){
+  /*private boolean addQueen(int r, int c){
     if(board[r][c]==0){
       board[r][c]=-1;
       addMarks(r,c,true);
@@ -13,8 +22,14 @@ public class QueenBoard{
     }
     else
     return false;
+  }*/
+  private boolean findSlope(int x, int y, String xy){
+    int a = Character.getNumericValue(xy.charAt(0));
+    int b = Character.getNumericValue(xy.charAt(1));
+    if( x == a || y == b || Math.abs(y-b) == Math.abs(x-a)) return false;
+    return true;
   }
-  private void addMarks(int r, int c, boolean add){
+  /*private void addMarks(int r, int c, boolean add){
     int changer = (add) ? 1 : -1;
     for(int i=1;c+i<board.length;i++){
       if(r-i>-1)
@@ -23,16 +38,48 @@ public class QueenBoard{
       if(r+i<board.length)
       board[r+i][c+i]+=changer;
     }
+  }*/
+  private boolean addQueen(int r, int c){
+		for(String str: illegalPoints){
+			if(! findSlope(r,c,str))
+			return false;
+
+		}
+		illegalPoints.add(r + "" +c);
+		return true;
+	}
+  private boolean removeQueen(String xy){
+    if(illegalPoints.contains(xy)){
+      illegalPoints.remove(xy);
+      return true;
+    }
+    return false;
   }
-  private boolean removeQueen(int r, int c){
+
+  /*private boolean removeQueen(int r, int c){
     if(board[r][c]==-1){
       board[r][c]=0;
       addMarks(r,c,false);
       return true;
     }
     return false;
-  }
+  }*/
   public String toString(){
+    String str = "";
+    for(int i=0; i<size; i++){
+      for(int j=0; j<size; j++){
+        if(illegalPoints.contains(i + "" + j)){
+          str += "Q ";
+        }
+        else {
+          str += "_ ";
+        }
+      }
+      str += "\n";
+    }
+    return str;
+  }
+  /*public String toString(){
     //TWO-D Array Version
     String str = "";
     for(int i=0;i<board.length;i++){
@@ -47,18 +94,54 @@ public class QueenBoard{
       str+="\n";
     }
     return str;
-  }
-
+  }*/
   public boolean solve(){
+		if(illegalPoints.size() > 0) throw new IllegalStateException("There is already a Queen");
+		return 1 == solveH(0, false, 0);
+
+	}
+  /*public boolean solve(){
     for(int [] row: board){
       for(int value: row){
         if(value!=0)throw new IllegalStateException("Board has a non-0 Value");
       }
     }
     return 1==solveH(0,0, false, 0);
+  }*/
+  private int solveH(int row, boolean retNumSolutions, int sum){
+    if(row==size && illegalPoints.size()==0){
+        System.out.println(this);
+        return sum;
+    }
+    if(illegalPoints.size() == size){
+      sum++;
+      if(retNumSolutions==false){
+        System.out.println(this);
+        return sum;
+      }
+      else{
+        String str = illegalPoints.get(illegalPoints.size() - 1);
+        int nextRow = Character.getNumericValue(str.charAt(0)) + 1;
+        if(! removeQueen(str)) return -1;
+        return solveH(nextRow, retNumSolutions, sum);
+      }
+    }
+    int column = illegalPoints.size();
+    int i = row;
+    while(i < size){
+      if(addQueen(i, column)) break;
+      i++;
+    }
+    if(i == size){
+      String str = illegalPoints.get(illegalPoints.size()-1);
+      int newStart = Character.getNumericValue(str.charAt(0)) + 1;
+      removeQueen(str);
+      return solveH(newStart, retNumSolutions, sum);
+    }
+    else
+      return solveH(0, retNumSolutions, sum);
   }
-
-  private int solveH(int row, int column, boolean integer, int sum){
+  /*private int solveH(int row, int column, boolean integer, int sum){
     if(row==board.length && column==0){
         return sum;
     }
@@ -89,13 +172,17 @@ public class QueenBoard{
         }
       }
       return solveH(0,column+1,integer,sum);
-    }
+    }*/
     public int countSolutions(){
+      if(illegalPoints.size() > 0) throw new IllegalStateException("There is already a Queen");
+      return solveH(0, true, 0);
+    }
+    /*public int countSolutions(){
       for(int [] row: board){
         for(int value: row){
           if(value!=0)throw new IllegalStateException("Board has a non-0 Value");
         }
       }
       return solveH(0,0, true, 0);
-    }
+    }*/
   }
--- QueenBoardDriver.java
+++ QueenBoardDriver.java
@@ -1,8 +1,8 @@
 public class QueenBoardDriver{
 	public static void main(String [] args){
 		QueenBoard chess = new QueenBoard(8);
+		System.out.println(chess.countSolutions());
 		System.out.println(chess.solve());
 		System.out.println(chess);
-		//System.out.println(chess.countSolutions());
 	}
 }
