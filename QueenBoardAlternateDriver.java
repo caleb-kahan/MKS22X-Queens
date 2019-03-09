5f05bc8db1bea8cc38fb4483f8f68b3ce17dc681
Caleb Kahan <ckahan00@stuy.edu>
2019-03-09 03:28:58 +0000

    useless

--- QueenBoardAlternateDriver.java
+++
@@ -1,7 +0,0 @@
-public class QueenBoardAlternateDriver{
-	public static void main(String [] args){
-		QueenBoardAlternate chess = new QueenBoardAlternate(8);
-		System.out.println(chess.countSolutions());
-		System.out.println(chess.solve());
-	}
-}
